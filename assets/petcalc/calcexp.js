21af3736ef85a14ba833fc7a1ca623e8e30b34c7
Varefox <taigengraham@gmail.com>
2023-02-18 11:50:53 -0800

    Merge branch 'Main' of https://github.com/Varefox/ChimeraTools into Main

--- assets/petcalc/calcexp.js
+++ assets/petcalc/calcexp.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 //@ts-nocheck
-=======
-// Required experience indexed to match level
-const expValues = [0, 95, 120, 150, 180, 215, 255, 295, 340, 385, 550, 670, 800, 945, 1105, 1275, 1455, 1645, 1850, 2070, 2665, 3125, 3625, 4155, 4725, 5330, 5970, 6650, 7360, 8110, 10230, 11890, 13665, 15565, 17580, 19715, 21975, 24350, 26850, 29465, 36040, 41815, 48200, 55175, 62770, 70980, 79810, 89270, 99385, 110155, 130935, 145000, 173670, 199045, 227240, 258285, 292325, 329445, 369720, 413260, 826525, 1653050, 2479575, 3306100, 4132625, 4959150, 5785675, 6612200, 7438725, 8265250];
->>>>>>> 842d7597974398c2128fa05854caf8a3923e2629
 
 const lv = document.getElementById("level");
 const exp = document.getElementById("currentExp");
 const lvGoal = document.getElementById("levelGoal");
 const expGoal = document.getElementById("expNeededText");
 
-<<<<<<< HEAD
 const pills = document.getElementById("currentPills");
 const reverseLv = document.getElementById("reverseLevel");
 const reverseXP = document.getElementById("reverseExp");
@@ -23,10 +17,6 @@ const skycharge = 20000;
 
 const calcExp = () => {
     const expValues = [0, 95, 120, 150, 180, 215, 255, 295, 340, 385, 550, 670, 800, 945, 1105, 1275, 1455, 1645, 1850, 2070, 2665, 3125, 3625, 4155, 4725, 5330, 5970, 6650, 7360, 8110, 10230, 11890, 13665, 15565, 17580, 19715, 21975, 24350, 26850, 29465, 36040, 41815, 48200, 55175, 62770, 70980, 79810, 89270, 99385, 110155, 130935, 145000, 173670, 199045, 227240, 258285, 292325, 329445, 369720, 413260, 826525, 1653050, 2479575, 3306100, 4132625, 4959150, 5785675, 6612200, 7438725, 8265250];
-=======
-
-const calcExp = () => {
->>>>>>> 842d7597974398c2128fa05854caf8a3923e2629
     
     let count = 0; // This will sum up exp per level for a given range.
     for (let i = parseInt(lv.value, 10); i <= parseInt(lvGoal.value, 10) - 1; i++) {
@@ -36,12 +26,6 @@ const calcExp = () => {
 
     expGoal.innerText = parseInt(count).toLocaleString("en-US");
 
-<<<<<<< HEAD
-=======
-    // EXP amounts for pills
-    const purity = 50000;
-    const skycharge = 20000;
->>>>>>> 842d7597974398c2128fa05854caf8a3923e2629
     
     let purCt = Math.floor(count / purity);
     let skyCt = Math.ceil((count - (purity * purCt)) / skycharge);
@@ -51,7 +35,6 @@ const calcExp = () => {
     
     // Update the results
     document.getElementById("purity-needed").innerText = purCt;
-<<<<<<< HEAD
     document.getElementById("skycharge-needed").innerText = skyCt;       
 }
 
@@ -93,14 +76,6 @@ const flipback = () => {
 
 // Limits the values so that we don't have errors
 const limiter = (input) => {
-=======
-    document.getElementById("skycharge-needed").innerText = skyCt;
-        
-}
-
-// Limits the values so that we don't have errors
-limiter = (input) => {
->>>>>>> 842d7597974398c2128fa05854caf8a3923e2629
     if (input.value > 70) input.value = 70;
     if (lv.value >= lvGoal.value) lv.value = lvGoal.value - 1;
     calcExp();
--- sharedlib/alias.js
+++
@@ -1,4 +0,0 @@
-const query = (qStr, parent = document) => parent.querySelector(qStr);
-const style = (element, style) => (Object.assign(element.style, style), element);
-const tag = (tag, props = {}) => Object.assign(document.createElement(tag), props);
-const append = (parent, ...sibs) => sibs.reduce((p, sib) => (p.appendChild(sib), p), parent);
