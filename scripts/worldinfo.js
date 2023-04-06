f26c149bde8f071856a1c76ff1538ac04173351a
Varefox <taigengraham@gmail.com>
2023-04-06 11:08:06 -0700

    created worldinfo app, unfinished, working order.

--- scripts/worldinfo.js
+++ scripts/worldinfo.js
@@ -1,144 +1,178 @@
-const loadListData = async () => {
-    try {
-        const response = await fetch("/ChimeraTools/scripts/worldinfo.json");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching JSON data:", error);
-    }
-};
+$.getScript('scripts/widata.js', function () {
+    console.log("widata.js script loaded");
 
-const isObject = (value) =>
-    typeof value === "object" && value !== null && !Array.isArray(value);
+    const state = {
+        selectedItems: {}
+    };
 
-const completedItems = [];
+    const listSelectors = {
+        grid1: '#grid1-list',
+        grid2: '#grid2-list',
+        grid3: '#grid3-list'
+    };
 
-const updateCompletedList = () => {
-    $("#completed-items").empty();
-    completedItems.forEach((item) => {
-        const listItem = $("<li>").text(item);
-        $("#completed-items").append(listItem);
-    });
-};
+    const isObject = (value) => typeof value === "object" && value !== null && !Array.isArray(value);
 
-const onCheckboxChange = (e) => {
-    const checkbox = $(e.target);
-    const item = checkbox.data("item");
-    if (checkbox.is(":checked")) {
-        $("#selected-items").append($("<li>").text(item));
-        completedItems.push(item);
-    } else {
-        completedItems.splice(completedItems.indexOf(item), 1);
-        $("#selected-items li")
-            .filter(function () {
-                return $(this).text() === item;
-            })
-            .remove();
-    }
-    updateCompletedList();
-};
-
-const populateList = (data, category) => {
-    const listContainer = $("#list-container");
-    const listTitle = $("#list-title");
-    const list = $("#list");
-
-    listTitle.text(category.charAt(0).toUpperCase() + category.slice(1));
-    list.empty();
-
-    if (data[category]) {
-        if (isObject(data[category])) {
-            Object.keys(data[category]).forEach((subcategory) => {
-                const subcategoryTitle = $("<h3>").text(subcategory);
-                list.append(subcategoryTitle);
-                const subcategoryData = data[category][subcategory];
-                if (Array.isArray(subcategoryData)) {
-                    subcategoryData.forEach((item) => {
-                        const listItem = $("<li>");
-                        const checkbox = $("<input>", {
-                            type: "checkbox",
-                            class: "custom-checkbox",
-                        })
-                            .data("item", item)
-                            .on("change", onCheckboxChange);
-                        const label = $("<label>").append(checkbox).append(item);
-                        listItem.append(label);
-                        list.append(listItem);
-                    });
-                } else if (isObject(subcategoryData)) {
-                    Object.keys(subcategoryData).forEach((key) => {
-                        if (Array.isArray(subcategoryData[key])) {
-                            const propertyTitle = $("<h4>").text(key);
-                            list.append(propertyTitle);
-                            subcategoryData[key].forEach((item) => {
-                                const listItem = $("<li>").text(item);
-                                list.append(listItem);
-                            });
-                        } else {
-                            const propertyItem = $("<li>").text(`${key}: ${subcategoryData[key]}`);
-                            list.append(propertyItem);
-                        }
-                    });
-                }
+    const createCheckbox = (item, category) => {
+        const checkbox = $('<input type="checkbox">')
+            .addClass("checkbox")
+            .on("change", function () {
+                updateSelectedItems(this, item, category);
             });
+
+        if (state.selectedItems[category]?.includes(item)) {
+            checkbox.prop("checked", true);
+        }
+
+        return checkbox;
+    };
+    
+    const createCategoryTitle = (text) => {
+        return $("<li>")
+            .addClass("category-title")
+            .text(text);
+    };
+
+    const createLabel = (item, category) => {
+        const itemName = (item instanceof Attendant) ? item.name : item;
+        return $('<label>').text(itemName).prepend(createCheckbox(item, category));
+    };
+
+
+    const createListItem = (item, category) => {
+        const listItem = $("<li>").append(createLabel(item, category));
+        return listItem;
+    };
+
+    const populateList = (data, category) => {
+        const list = $("#list");
+        list.empty();
+
+        const processItems = (items) => {
+            const categorizedItems = {};
+
+            items.forEach((item) => {
+                const itemCategory = getItemCategory(item);
+                if (!categorizedItems[itemCategory]) {
+                    categorizedItems[itemCategory] = [];
+                }
+                categorizedItems[itemCategory].push(item);
+            });
+
+            for (let subcategory in categorizedItems) {
+                const categoryTitle = createCategoryTitle(subcategory);
+                list.append(categoryTitle);
+                categorizedItems[subcategory].forEach((item) => {
+                    const listItem = createListItem(item, category);
+                    list.append(listItem);
+                });
+            }
+        };
+
+        if (Array.isArray(data[category])) {
+            processItems(data[category]);
+        } else if (isObject(data[category])) {
+            if (category === 'attendants') {
+                processItems(attendants);
+            } else {
+                for (let key in data[category]) {
+                    if (Array.isArray(data[category][key])) {
+                        data[category][key].forEach((item) => {
+                            const listItem = createListItem(item, category);
+                            list.append(listItem);
+                        });
+                    } else if (isObject(data[category][key]) && data[category][key].name) {
+                        const listItem = createListItem(data[category][key].name, category);
+                        list.append(listItem);
+                    }
+                }
+            }
         } else {
-            data[category].forEach((item) => {
+            list.append($("<li>").text("No data available for this category."));
+        }
+
+        $("#list-title").text(category.charAt(0).toUpperCase() + category.slice(1));
+    };
+
+
+    const updateSelectedItems = (checkbox, item, category) => {
+        const selectedItemsList = $("#selected-items");
+
+        if (!state.selectedItems[category]) {
+            state.selectedItems[category] = [];
+        }
+
+        if (checkbox.checked) {
+            if (!state.selectedItems[category].includes(item)) {
+                state.selectedItems[category].push(item);
                 const listItem = $("<li>").text(item);
-                list.append(listItem);
+                selectedItemsList.append(listItem);
+            }
+        } else {
+            const index = state.selectedItems[category].indexOf(item);
+            if (index > -1) {
+                state.selectedItems[category].splice(index, 1);
+                selectedItemsList.find(`:contains(${item})`).remove();
+            }
+        }
+        updateGrid3();
+    };
+
+    const getItemCategory = (item) => {
+        if (item instanceof Attendant) {
+            return item.village;
+        } else if (item.tier) {
+            return `Tier ${item.tier}`;
+        } else if (item.category) {
+            return item.category;
+        } else {
+            return "Uncategorized";
+        }
+    };
+
+    const updateGrid3 = () => {
+        const grid3 = $(listSelectors.grid3);
+        grid3.empty();
+        const categorizedItems = {};
+
+        for (let category in state.selectedItems) {
+            state.selectedItems[category].forEach((item) => {
+                const itemCategory = getItemCategory(item);
+                if (!categorizedItems[itemCategory]) {
+                    categorizedItems[itemCategory] = [];
+                }
+                categorizedItems[itemCategory].push(item);
             });
         }
-    } else {
-        list.append($("<li>").text("No data available for this category."));
-    }
-};
 
-(async () => {
-    const data = await loadListData();
-
-    $("button[data-category]").on("click", function () {
-        const category = $(this).data("category");
-        populateList(data, category);
-    });
-
-    $("#show-completed-items").on("click", function () {
-        $("#list-title").text("Completed Items");
-        $("#list").empty().append($("#completed-items").clone().children());
-    });
-
-    $("#print-button").on("click", function () {
-        const printContents = $("#selected-items").html();
-        const originalContents = document.body.innerHTML;
-        document.body.innerHTML = `<ul style="font-size: 20px;">${printContents}</ul>`;
-        window.print();
-        document.body.innerHTML = originalContents;
-    });
-})();
+        for (let category in categorizedItems) {
+            const categoryTitle = $("<li>").addClass("category-title").text(category);
+            grid3.append(categoryTitle);
+            categorizedItems[category].forEach((item) => {
+                const itemName = item instanceof Attendant ? item.name : item;
+                const listItem = $("<li>").text(itemName);
+                grid3.append(listItem);
+            });
+        }
+    };
 
 
-// attendant situation
-function attendantCheckboxHandler(attendant, food, stones) {
-    const list = document.getElementById("grid3-list");
-    const listItem = document.createElement("li");
-    listItem.textContent = `${attendant} - ${food} - ${stones} stones`;
-    list.appendChild(listItem);
-}
+    (async () => {
+        const data = await loadListData();
+        console.log("Data loaded:", data);
 
-const attendantsList = document.getElementById("attendants-list");
-for (const village in attendantsData) {
-    attendantsData[village].forEach(attendant => {
-        const listItem = document.createElement("li");
-
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.addEventListener("change", () => {
-            attendantCheckboxHandler(attendant, food, stones);
+        $("button[data-category]").on("click", function () {
+            const category = $(this).data("category");
+            populateList(data, category);
+            updateGrid3();
         });
 
-        const label = document.createElement("label");
-        label.textContent = attendant;
-
-        listItem.appendChild(checkbox);
-        listItem.appendChild(label);
-        attendantsList.appendChild(listItem);
-    });
-}
+        $("#print-button").on("click", function () {
+            const printContents = $("#selected-items").html();
+            const originalContents = document.body.innerHTML;
+            document.body.innerHTML = `<ul style="font-size: 20px;">${printContents}</ul>`;
+            window.print();
+            document.body.innerHTML = originalContents;
+        });
+    })();
+});
